027d3b348b598f22e9c6c11077b96287df7205d0
umeshkhatiwada13 <umeshkhatiwada13@gmail.com>
2023-10-10 23:02:42 -0400

    solve contains duplicate problem in O(n) time

---
+++ src/Neetcode150/Arrays/ContainsDuplicate217Easy.java
@@ -0,0 +1,29 @@
+package Neetcode150.Arrays;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author umeshkhatiwada13@infodev
+ * @project Leetcode
+ * @created 10/10/2023 - 17:44
+ */
+public class ContainsDuplicate217Easy {
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer, Boolean> countMap = new HashMap<>();
+        Arrays.sort(nums);
+        for (int a : nums) {
+            if (countMap.containsKey(a)) {
+                return true;
+            } else {
+                countMap.put(a, Boolean.TRUE);
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
